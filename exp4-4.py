fa5457c5acfaa85383b5f5000242e8b70b97e448
changchujing <104438685+changchujing@users.noreply.github.com>
2022-06-07 21:08:49 +0800

    Update exp4-4.py

--- exp4-4.py
+++ exp4-4.py
@@ -1,7 +1,10 @@
-r=int(input("请输入圆半径："))
-a=2*3.14*r
-print("周长=")
-print(a)
-b=3.14**r
-print("面积=")
-print(b)
+year=int(input("請輸入年份："))
+print("您输入的是",year,"年")
+x=year%4
+y=year%100
+z=year%400
+if  x==0:
+   if y!=0 or z==0:
+    print("%s是閏年"%year)
+else:
+    print("%s不是閏年"%year)
